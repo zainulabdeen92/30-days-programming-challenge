cb1574c2ad1ccfcdb1acee9d0d8ff2417b17ac42
zainulabdeen92 <161713884+zainulabdeen92@users.noreply.github.com>
2024-04-22 23:59:18 -0700

    Add files via upload

---
+++ 30 Days Python programming challenge/Day05/int.py
@@ -0,0 +1,9 @@
+number = int(input("Enter your number: "))
+
+if number > 0:
+    print("your number is positive")
+elif number < 0:
+    print("your number is negative")
+else:
+    print("your number is zero")
+    
---
+++ 30 Days Python programming challenge/Day05/loop.py
@@ -0,0 +1,3 @@
+for i in range(1,11):
+    square = i **2 
+    print (square)
